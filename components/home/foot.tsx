36fb3974f50d916d37ae6d8e3572e5fade0e46cb
Mohamed Omar <Mohamed12@live.co.uk>
2021-08-14 16:50:34 +0100

    Update to latest plugins, enable eslint/prettier

--- components/home/foot.tsx
+++ components/home/foot.tsx
@@ -1,13 +1,13 @@
 const HomeFooter = () => {
   return (
-    <footer className={'w-full h-24 border-t border-accent-2 flex justify-center items-center'}>
+    <footer className='w-full h-24 border-t border-accent-2 flex justify-center items-center'>
       <pre>
         Snowdrop by Mohamed Omar
         <br />
-        NextJS, <span className={'line-through'}>Strapi, Postgres</span>
+        NextJS, <span className='line-through'>Strapi, Postgres</span>
       </pre>
     </footer>
-  )
-}
+  );
+};
 
-export default HomeFooter
+export default HomeFooter;
